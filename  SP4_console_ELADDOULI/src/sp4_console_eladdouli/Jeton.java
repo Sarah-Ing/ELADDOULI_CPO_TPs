0c052fbd8ee7cdc1d2b5ed3ffb4670f56194cac3
Sarah <sasou.eladdouli@orange.fr>
2021-10-25 17:14:47 +0200

    Projet : Classe Jeton, semble finie version du 25/10

---
+++  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Jeton.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_eladdouli;
+
+/**
+ *
+ * @author sarah
+ */
+public class Jeton 
+{
+    String couleur;
+    
+    
+public Jeton (String c) 
+{
+   couleur = c ;
+  
+}
+public String lireCouleur () {
+    return (couleur);
+}
+
+}
