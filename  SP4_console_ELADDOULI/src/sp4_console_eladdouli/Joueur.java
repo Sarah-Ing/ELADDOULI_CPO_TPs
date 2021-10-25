587e97c8700f97a6d5f31d2eb6b0de5077ab7a4e
Sarah <sasou.eladdouli@orange.fr>
2021-10-25 17:16:08 +0200

    Projet : Classe joueur, les méthodes ne sont pas commencées version du 25/10

---
+++  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Joueur.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_eladdouli;
+
+/**
+ *
+ * @author sarah
+ */
+public class Joueur 
+{
+    String nom;
+    String Couleur;
+    Jeton [] ListeJetons = new Jeton[21]; // int ????
+    int nombreJetonsRestants; // nombreJetonsRestants: nombre de jetons restant en possession du joueur, correspondant à la taille effective de ListeJetons
+
+}
