c62372ff0d358e4352ab92100faaae1e14033ba3
Sarah <sasou.eladdouli@orange.fr>
2021-11-12 10:55:43 +0100

    SuperPuissance4 : Grille ajouterJetonColonne FINI etreRemplie DEBUT
    V1.0

---  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Grille.java
+++  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Grille.java
@@ -9,5 +9,60 @@ package sp4_console_eladdouli;
  * @author sarah
  */
 public class Grille {
+    Cellule CellulesJeu[][] = new Cellule[6][7];
+    
+
+    public Grille()
+    {CellulesJeu = new Cellule[6][7];}
+    
+    public boolean ajouterJetonDansColonne(Jeton j, int col)
+    {
+    if (CellulesJeu[0][col] != null) 
+    {return false;}
+    else 
+    {
+        for(int i= 0; i<= 5; i++)
+        {
+            
+            if(CellulesJeu[i][col] != null)
+            {CellulesJeu[i-1][col].affecterJeton(j);}
+        }  return true;
+    }
+   
+    }
+    
+    public boolean etreRemplie()
+    {
+    for (int)
+    }
+    
+    public void viderGrille()
+    {}
+    
+    public void afficherGrilleSurConsole()
+    {}
+    
+    public boolean celluleOccupee(int,int)
+    {}
+    
+    public String lireCouleurDuJeton(int, int)
+    {}
+    
+    public boolean etreGagnantePourJoueur(Joueur)
+    {}
+    
+    //public void tasserGrille(int){}
+    
+    public boolean colonneRemplie()
+    {}
+    
+    //public boolean placerTrouNoir(int, int){}
+    
+    //public boolean placerDesintegrateur(int, int){}
+    
+   // public boolean supprimerJeton(int, int){}
+    
+    //public boolean recupererJeton(int, int){}
+    
     
 }
