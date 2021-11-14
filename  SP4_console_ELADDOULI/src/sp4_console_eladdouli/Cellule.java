0e3a9c5af35d932fc559f86e1ea401a76752046e
Sarah <sasou.eladdouli@orange.fr>
2021-11-14 17:34:04 +0100

    SuperPuissance4 : Cellule V1.0
    14/11 finni j'ai trouve l'erreur 
    attention "couleur" en minuscule

---  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Cellule.java
+++  SP4_console_ELADDOULI/src/sp4_console_eladdouli/Cellule.java
@@ -29,7 +29,7 @@ public class Cellule {
         {return false;}
     }
     
-    public string lireCouleurDuJeton() 
+    public String lireCouleurDuJeton() 
     {
     if(jetonCourant != null)
         {
