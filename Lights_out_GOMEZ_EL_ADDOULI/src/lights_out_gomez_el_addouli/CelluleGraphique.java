2c955bc68902832fec9b63f80eb99c9441c2277d
Natha <Natha@DESKTOP-IMFIRBI>
2021-11-30 18:53:08 +0100

    fin de cours

---
+++ Lights_out_GOMEZ_EL_ADDOULI/src/lights_out_gomez_el_addouli/CelluleGraphique.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lights_out_gomez_el_addouli;
+
+/**
+ *
+ * @author Natha
+ */
+public class CelluleGraphique {
+    
+    
+    Grille CelluleAssociee;
+    
+    public CelluleGraphique(Grille uneCellule){
+            CelluleAssociee = uneCellule;
+    }
+    
+}
