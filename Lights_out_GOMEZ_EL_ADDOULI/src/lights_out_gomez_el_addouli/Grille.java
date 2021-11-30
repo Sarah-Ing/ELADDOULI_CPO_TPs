a97ff1b9b719829972bf5eb35a35f0b81a85cb64
Sarah <sasou.eladdouli@orange.fr>
2021-11-30 17:21:25 +0100

    Création de la fenetre de jeu Suppression de lights out

---
+++ Lights_out_GOMEZ_EL_ADDOULI/src/lights_out_gomez_el_addouli/Grille.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lights_out_gomez_el_addouli;
+
+/**
+ *
+ * @author sarah
+ */
+public class Grille 
+{
+ // Attributs :
+
+Grille[][] GrilleJeu = new Grille[5][5];
+
+// Méthodes :
+
+public Grille()
+    {
+        
+    }
+}
