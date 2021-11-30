504af8cc5f74d3ff0319c2e452c89b3b797fe470
Sarah <sasou.eladdouli@orange.fr>
2021-11-30 16:26:13 +0100

    MISE EN PLACE

---
+++ Lights_out_GOMEZ_EL_ADDOULI/src/lights_out_gomez_el_addouli/Lights_out_GOMEZ_EL_ADDOULI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lights_out_gomez_el_addouli;
+
+/**
+ *
+ * @author sarah
+ */
+public class Lights_out_GOMEZ_EL_ADDOULI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    
+    {
+        
+    }
+    
+}
