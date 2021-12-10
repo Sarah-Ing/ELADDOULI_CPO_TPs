d5aaa63310ab5e86481bb6223b548abd99e63f0c
Natha <Natha@DESKTOP-IMFIRBI>
2021-12-10 16:01:32 +0100

    Création class Partie

---
+++ Lights_out_GOMEZ_EL_ADDOULI/src/lights_out_gomez_el_addouli/Partie.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lights_out_gomez_el_addouli;
+
+/**
+ *
+ * @author Natha
+ */
+public class Partie {
+    
+}
